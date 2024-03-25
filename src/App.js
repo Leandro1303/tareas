a59844b5abb0abf9dec50a92bc56335f1a9f72f2
Leandro1303 <100365419@est.uasd.edu.do>
2024-03-25 00:43:23 -0400

    Ya realice el primer reto y funciona nitido

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
+import freeCodeCampLogo from './imagenes/freecodecamp-logo.png';
+import { useState } from 'react';
+import TareaFormulario from './componentes/TareaFormulario';
+import Tarea from './componentes/Tarea';
 
 function App() {
+  const [tareas, setTareas] = useState([]);
+
+  const agregarTarea = tarea => {
+    if (tarea.texto.trim()) {
+      tarea.texto = tarea.texto.trim();
+      const tareasActualizadas = [tarea, ...tareas];
+      setTareas(tareasActualizadas);
+    }
+  }
+
+  const eliminarTarea = id => {
+    const tareasActualizadas = tareas.filter(tarea => tarea.id !== id);
+    setTareas(tareasActualizadas);
+  }
+
+  const completarTarea = id => {
+    const tareasActualizadas = tareas.map(tarea => {
+      if (tarea.id === id) {
+        tarea.completada = !tarea.completada;
+      }
+      return tarea;
+    });
+    setTareas(tareasActualizadas);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='aplicacion-tareas'>
+      <div className='freecodecamp-logo-contenedor'>
+        <img 
+          src={freeCodeCampLogo} 
+          className='freecodecamp-logo'
+          alt=''
+        />
+      </div>
+      <div className='tareas-lista-principal'>
+        <h1>Mis Tareas</h1>
+        <TareaFormulario onSubmit={agregarTarea} />
+        <div className='tareas-lista-contenedor'>
+          {
+            tareas.map((tarea) =>
+              <Tarea
+                key={tarea.id}
+                id={tarea.id} 
+                texto={tarea.texto}
+                completada={tarea.completada}
+                completarTarea={completarTarea}
+                eliminarTarea={eliminarTarea}
+              />
+            ) 
+          }
+        </div>
+      </div>
     </div>
   );
 }
